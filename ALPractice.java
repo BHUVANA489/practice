919ca32f2bacc28ff89ef7276042a17ad65343b8
Muttangi Bhuvaneswari <bhuvanamuttangi@gmail.com>
2026-08-29 12:30:07 +0530

    Add files via upload

---
+++ ALPractice.java
@@ -0,0 +1,23 @@
+package com.collectionPractice;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class ALPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] arr = new Integer[] {1,3,5,7,9};
+		ArrayList<Integer> al1= new ArrayList<>(Arrays.asList(arr));
+		System.out.println(al1);
+		System.out.println(al1.get(1));
+//		ArrayList<Integer> al1 = new ArrayList<>();
+//		al1.add(null);
+//		al1.add(5);
+//		al1.add(0,1);
+//        al1.size();
+//		
+//		
+//		System.out.println(al1);
+
+	}
+
+}
