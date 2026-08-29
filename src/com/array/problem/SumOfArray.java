d9d1799685cef71ee77004057ef6e2f3a9d6cf68
BHUVANA489 <bhuvanamuttangi@gmail.com>
2026-08-29 12:44:55 +0530

    Added Sumofarray program

---
+++ src/com/array/problem/SumOfArray.java
@@ -0,0 +1,22 @@
+package com.array.problem;
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		
+		        // Declare and initialize the array
+		        int[] arr = {10, 20, 30, 40, 50};
+
+		        int sum = 0;
+
+		        // Loop through the array and calculate the sum
+		        for (int i = 0; i < arr.length; i++) {
+		            sum += arr[i];
+		        }
+
+		        // Display the result
+		        System.out.println("Sum of array elements = " + sum);
+		    }
+		}
+
+	
